d19bf243000b377ba52618b160f7c5c241654778
Ishan <contact@ishansharma.com>
2018-04-14 15:38:20 -0500

    Adding prompt to select which algorithm to choose

--- __main__.py
+++ __main__.py
@@ -1,48 +1,18 @@
-import cv2
-import numpy as np
-import time
+from harris_corner_detection import basic as hc
+from harris_corner_detection import subpixel as hsp
 
-filename = '../../dataset/Hands/Hand_0000083.jpg'
-img = cv2.imread(filename)
-# small = cv2.resize(img, (0, 0), fx=0.9, fy=0.9)
-small = img
-gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
+choice_message = """
+Which program should I run?
 
-# basic feature detection
-# #######################
+1. Basic Harris Detection
+2. Harris Detection with Subpixel Accuracy
+3. Shi Tomasi
+"""
 
-# gray = np.float32(gray)
-# dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-#
-# # result dilated for marking corners
-# dst = cv2.dilate(dst, None)
-#
-# # threshold for optimal value (may need to experiment with this)
-# small[dst > 0.004 * dst.max()] = [0, 0, 255]
-#
-# cv2.imshow('dst', small)
-#
-# if cv2.waitKey(0) & 0xff == 27:
-#     cv2.destroyAllWindows()
+choice = int(input(choice_message))
 
-# subpixel - further refinement
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-dst = cv2.dilate(dst, None)
-ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
-dst = np.uint8(dst)
+if choice == 1:
+    hc.run()
 
-# find centroids
-ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
-
-# define the criteria to stop and refine the corners
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-corners = cv2.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
-
-# draw the centroids
-res = np.hstack((centroids, corners))
-res = np.int0(res)
-img[res[:, 1], res[:, 0]] = [0, 0, 255]
-img[res[:, 3], res[:, 2]] = [0, 255, 0]
-
-cv2.imwrite('out/subpixel' + str(time.time()) + '.png', img)
+if choice == 2:
+    hsp.run()
--- harris_corner_detection/subpixel.py
+++ harris_corner_detection/subpixel.py
@@ -1,31 +1,34 @@
-import cv2
-import numpy as np
 import time
 
-filename = '../../dataset/Hands/Hand_0000083.jpg'
-img = cv2.imread(filename)
-# small = cv2.resize(img, (0, 0), fx=0.9, fy=0.9)
-small = img
-gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
+import cv2
+import numpy as np
 
-# subpixel - further refinement
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-dst = cv2.dilate(dst, None)
-ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
-dst = np.uint8(dst)
 
-# find centroids
-ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+def run():
+    filename = '../../dataset/Hands/Hand_0000083.jpg'
+    img = cv2.imread(filename)
+    # small = cv2.resize(img, (0, 0), fx=0.9, fy=0.9)
+    small = img
+    gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
 
-# define the criteria to stop and refine the corners
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-corners = cv2.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
+    # subpixel - further refinement
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+    dst = cv2.dilate(dst, None)
+    ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
+    dst = np.uint8(dst)
 
-# draw the centroids
-res = np.hstack((centroids, corners))
-res = np.int0(res)
-img[res[:, 1], res[:, 0]] = [0, 0, 255]
-img[res[:, 3], res[:, 2]] = [0, 255, 0]
+    # find centroids
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
 
-cv2.imwrite('out/subpixel' + str(time.time()) + '.png', img)
+    # define the criteria to stop and refine the corners
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+    corners = cv2.cornerSubPix(gray, np.float32(centroids), (5, 5), (-1, -1), criteria)
+
+    # draw the centroids
+    res = np.hstack((centroids, corners))
+    res = np.int0(res)
+    img[res[:, 1], res[:, 0]] = [0, 0, 255]
+    img[res[:, 3], res[:, 2]] = [0, 255, 0]
+
+    cv2.imwrite('out/subpixel' + str(time.time()) + '.png', img)
